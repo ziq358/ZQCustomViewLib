a7130c3eb4e2f23e6ccbf63284f06c4b0695e0e1
wuyanqiang <yqwut@isoftstone.com>
2018-10-15 16:26:20 +0800

    增加 NoScrollViewPager

--- customviewlib/src/main/java/com/zq/customviewlib/NoScrollViewPager.java
+++ customviewlib/src/main/java/com/zq/customviewlib/NoScrollViewPager.java
@@ -18,20 +18,11 @@ public class NoScrollViewPager extends ViewPager {
         super(context, attrs);
     }
 
-    /**
-     * 1.dispatchTouchEvent一般情况不做处理
-     * ,如果修改了默认的返回值,子孩子都无法收到事件
-     */
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
         return super.dispatchTouchEvent(ev);   // return true;不行
     }
 
-    /**
-     * 是否拦截
-     * 拦截:会走到自己的onTouchEvent方法里面来
-     * 不拦截:事件传递给子孩子
-     */
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         if (isScroll) {
@@ -41,11 +32,6 @@ public class NoScrollViewPager extends ViewPager {
         }
     }
 
-    /**
-     * 是否消费事件
-     * 消费:事件就结束
-     * 不消费:往父控件传
-     */
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
         if (isScroll) {
