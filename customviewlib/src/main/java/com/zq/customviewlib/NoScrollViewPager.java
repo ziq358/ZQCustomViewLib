7a1ac3386e267214393c99ff6213aee2038996e1
wuyanqiang <yqwut@isoftstone.com>
2018-10-15 16:12:33 +0800

    增加 NoScrollViewPager

---
+++ customviewlib/src/main/java/com/zq/customviewlib/NoScrollViewPager.java
@@ -0,0 +1,61 @@
+package com.zq.customviewlib;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NoScrollViewPager extends ViewPager {
+    private boolean isScroll;
+
+    public NoScrollViewPager(@NonNull Context context) {
+        super(context);
+    }
+
+    public NoScrollViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * 1.dispatchTouchEvent一般情况不做处理
+     * ,如果修改了默认的返回值,子孩子都无法收到事件
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return super.dispatchTouchEvent(ev);   // return true;不行
+    }
+
+    /**
+     * 是否拦截
+     * 拦截:会走到自己的onTouchEvent方法里面来
+     * 不拦截:事件传递给子孩子
+     */
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (isScroll) {
+            return super.onInterceptTouchEvent(ev);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 是否消费事件
+     * 消费:事件就结束
+     * 不消费:往父控件传
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (isScroll) {
+            return super.onTouchEvent(ev);
+        } else {
+            return true;// 可行,消费,拦截事件
+        }
+    }
+
+    public void setScroll(boolean scroll) {
+        isScroll = scroll;
+    }
+}
